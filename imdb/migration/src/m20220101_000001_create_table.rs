ff58d128265a22e038d4e923fb56e1a97923bf75
Dmitry Blinov <dblinov76@gmail.com>
2023-10-30 15:29:05 +0200

    Imdb: add sqlite table for titles

---
+++ imdb/migration/src/m20220101_000001_create_table.rs
@@ -0,0 +1,50 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+	async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+		manager
+			.create_table(
+				Table::create()
+					.table(Titles::Table)
+					.if_not_exists()
+					.col(ColumnDef::new(Titles::Title).string().not_null())
+					.col(ColumnDef::new(Titles::Id).integer().not_null())
+					.to_owned(),
+			)
+			.await?;
+
+		manager
+			.create_index(
+				Index::create()
+					.if_not_exists()
+					.name("idx_title")
+					.table(Titles::Table)
+					.col(Titles::Title)
+					.to_owned(),
+			)
+			.await?;
+
+		Ok(())
+	}
+
+	async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+		manager
+			.drop_index(Index::drop().name("idx_title").table(Titles::Table).to_owned())
+			.await?;
+
+		manager.drop_table(Table::drop().table(Titles::Table).to_owned()).await?;
+
+		Ok(())
+	}
+}
+
+#[derive(DeriveIden)]
+enum Titles {
+	Table,
+	Title,
+	Id,
+}
